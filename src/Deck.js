a6c3173566af83d307f23b548d97eccd788556a8
Connor Ashley <ashleycm@vcu.edu>
2021-07-01 18:51:52 -0400

    included dealCard() async method that calls api for new random card from unique deck and adds the returned card object to this.state.cards

--- src/Deck.js
+++ src/Deck.js
@@ -10,6 +10,7 @@ class Deck extends Component {
             deckId: '',
             cards: []
         };
+        this.dealCard = this.dealCard.bind(this);
     }
     async componentDidMount() {
         let url = "https://deckofcardsapi.com/api/deck/new/shuffle";
@@ -17,12 +18,20 @@ class Deck extends Component {
         let deckId = response.data.deck_id;
         this.setState({ deckId  })
     }
+    async dealCard() {
+        let url = `https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw`;
+        let response = await axios.get(url);
+        let card = response.data.cards[0];
+        this.setState({
+            cards: [...this.state.cards, card]
+        })
+    }
     render () {
         return (
             <div className="Deck">
                 <h1>React Dice Demo</h1>
                 <Card />
-                <button>Gimme a Card!</button>
+                <button onClick={this.dealCard}>Gimme a Card!</button>
             </div>
         )
     }
