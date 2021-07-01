e58b5f217fcdc988e20b4f1436230177d3473a95
Connor Ashley <ashleycm@vcu.edu>
2021-07-01 18:25:33 -0400

    render most basic child comp with a div surrounding and img tag that will render card imgs

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Card extends Component {
+    render() {
+        return (
+            <div>
+                <img alt="card" src=""/>
+            </div>
+        )
+    }
+}
+
+export default Card;
