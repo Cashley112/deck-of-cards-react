ce55bdd0bd476f1344075c2e75439f3501bc68a3
Connor Ashley <ashleycm@vcu.edu>
2021-07-06 11:02:30 -0400

    refactored transform variables out of render method, and into the constructor

--- src/Card.js
+++ src/Card.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react'
 import './Card.css'
 
 class Card extends Component {
-    render() {
-        // pick random angle and coordinates for card to be dealt on
+    constructor(props) {
+        super(props);
         let angle = Math.random() * 90 - 45;
         let xPos = Math.random() * 40 - 20;
         let yPos = Math.random() * 40 - 20;
-        let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`
-        console.log(transform);
+        this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`
+    }
+    render() {
+        // pick random angle and coordinates for card to be dealt on
+        
         return (
-            <img style={{transform: transform}} className="Card" alt={this.props.id} src={this.props.imgUrl}/>
+            <img style={{transform: this._transform}} className="Card" alt={this.props.id} src={this.props.imgUrl}/>
         )
     }
 }
